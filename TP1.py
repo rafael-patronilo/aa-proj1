176e0d38552e0352a76d3bc4b5b6dd01064e43ca
Padraao <j.padrao@campus.fct.unl.pt>
2022-10-23 12:00:07 +0100

    lala

--- TP1.py
+++ TP1.py
@@ -1,2 +1,9 @@
 import part1
 import part2
+
+print("Part 1 - Regression: Collision avoidance in Space\n")
+part1.main()
+
+
+print("Part 2 - Classification: Detecting Bank note fraud\n")
+part2.main()
